af0f7258430735488f365ab659d8cd48d7d4a6c5
rohit-56 <rohitmou25@gmail.com>
2022-11-17 13:16:02 +0530

    add launchScreen gradients

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,29 +13,55 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var iconLogo: UIImageView!
     
+    let shape = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
      let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [UIColor.systemBlue.cgColor , UIColor.systemPurple.cgColor]
+        gradientLayer.colors = [UIColor.systemYellow.cgColor , UIColor.systemOrange.cgColor]
         gradientLayer.locations = [0,1]
         gradientLayer.type = .axial
         gradientLayer.frame = view.bounds
         iconLogo.layer.addSublayer(gradientLayer)
         
-        Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(changeStoryboard), userInfo: nil, repeats: false)
+        let circlePath = UIBezierPath(arcCenter: view.center, radius: 75, startAngle: 0, endAngle: .pi * 2, clockwise: true)
+        
+        shape.path = circlePath.cgPath
+        shape.lineWidth = 4
+        shape.strokeColor = UIColor.white.cgColor
+        shape.fillColor = UIColor.clear.cgColor
+        shape.strokeEnd = 0
+        view.layer.addSublayer(shape)
+        
        
+            
+            
+            Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.fillBorder), userInfo: nil, repeats: false)
+        
+        Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(changeStoryboard), userInfo: nil, repeats: false)
     }
+    
+    @objc func fillBorder(){
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+         animation.toValue = 1
+         animation.duration = 3
+         animation.isRemovedOnCompletion = false
+         animation.fillMode = .forwards
+         shape.add(animation , forKey: "strokeEnd")
+    }
+    
     @objc func changeStoryboard(){
         
-        background.layer.borderWidth = 1.0
-        background.layer.backgroundColor = UIColor.white.cgColor
+      
+        
         let storyboard = UIStoryboard(name: "Calculator", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "CalculatorViewController") as UIViewController // Explicit cast is required here.
         viewController.modalTransitionStyle = .coverVertical
         viewController.modalPresentationStyle = .fullScreen
         self.present(viewController, animated: true, completion: nil)
+         
     }
 
  
