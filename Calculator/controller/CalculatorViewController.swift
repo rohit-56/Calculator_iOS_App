e4ef65f0e0b62d9334b8a7ad78a8278bd88d0c09
rohit-56 <rohitmou25@gmail.com>
2022-11-26 18:50:02 +0530

    use of MARK to separate all actions

--- Calculator/controller/CalculatorViewController.swift
+++ Calculator/controller/CalculatorViewController.swift
@@ -15,16 +15,6 @@ class CalculatorViewController: UIViewController {
         // Do any additional setup after loading the view.
         learnSwift()
     }
-    
-
-    @IBAction func submit(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "gradientView", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "GradientViewController") as UIViewController // Explicit cast is required here.
-        viewController.modalTransitionStyle = .coverVertical
-        viewController.modalPresentationStyle = .fullScreen
-        self.present(viewController, animated: true, completion: nil)
-    }
-    
     func learnSwift() -> Int{
         let e1 = Employee(empName: "Rohit", designation: "Software Engineer")
         e1.email = "rohit@gmail.com"
@@ -32,6 +22,16 @@ class CalculatorViewController: UIViewController {
         
         print("Employee e1 Name \(String(describing: e1.empName)) and Employee e2 Name \(String(describing: e2.empName)) and again e1 \(String(describing: e1.email))")
         return 1;
+    }
+    
+    //MARK: - All Button Actions are here
+    
+    @IBAction func submit(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "gradientView", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "GradientViewController") as UIViewController // Explicit cast is required here.
+        viewController.modalTransitionStyle = .coverVertical
+        viewController.modalPresentationStyle = .fullScreen
+        self.present(viewController, animated: true, completion: nil)
     }
 
 }
